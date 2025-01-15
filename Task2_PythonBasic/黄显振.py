84fd9483581e5f63d9c923725e7ec6bd52395143
beichen1009 <1877395746@qq.com>
2025-01-15 19:45:43 +0800

    Add files via upload

---
+++ Task2_PythonBasic/黄显振.py
@@ -0,0 +1,35 @@
+import math
+def average(data,count):
+    sum=0
+    for element in data:
+        sum+=int(element)
+    return sum/count
+
+def variance(data, count, average):
+    sum=0
+    for element in data:
+        element=int(element)
+        sum+=(element-average)**2
+    return math.sqrt(sum/count)
+
+in_file=open("data.txt", "r", encoding="UTF-8")
+
+data=[]
+for line in in_file:
+    line=line.strip()
+    temp=line.split(" ")
+    data.extend(temp)
+
+count=len(data)
+average=average(data, count)
+variance=variance(data, count, average)
+
+in_file.close()
+
+out_file=open("result.txt","w", encoding="UTF-8")
+
+out_file.write(f"数据个数：{count}\n")
+out_file.write(f"均值：{average}\n")
+out_file.write(f"方差：{variance}\n")
+
+out_file.close()
